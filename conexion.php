a30ae9b5375f08a14089b7978e4e3f1026d31841
Nyan 011 <venteddiagram@gmail.com>
2022-11-19 21:46:28 -0700

    agregar variables de conexion a conexion.php

---
+++ conexion.php
@@ -0,0 +1,10 @@
+<?php
+    $server = "localhost";
+    $user = "root";
+    $pwd = "";
+
+    $con = new PDO("mysql:host=$server; dbname=NeedPc", $user, $pwd);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $con = null;
+    ?>
